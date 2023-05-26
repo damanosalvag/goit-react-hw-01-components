0fa1b26009791d0fce6ba75d844a18d96d6ffc47
Dairo Manosalva <38769711+damanosalvag@users.noreply.github.com>
2023-05-26 18:55:36 -0500

    task 1 ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import user from '../data/user.json';
-import {Profile} from './Perfil/index'
+import { Profile } from './Perfil/index';
+import styles from './Perfil/Perfil.module.css';
+
 
 export const App = () => {
   return (
-    <div className="container">
+    <div className={styles["container"]}>
       <Profile {...user} />
     </div>
   );
--- src/components/Perfil/Perfil.jsx
+++ src/components/Perfil/Perfil.jsx
@@ -1,26 +1,39 @@
 import styles from './Perfil.module.css';
+import PropTypes from 'prop-types';
 
 export const Profile = ({ username, tag, location, avatar, stats }) => (
     <div className={styles['profile']}>
         <div className={styles['description']}>
             <img src={avatar} alt={username} className={styles["avatar"]}/>
-            <p className="name">{username}</p>
-            <p className="tag">@{tag}</p>
-            <p className="location">{location}</p>
+            <p className={styles["name"]}>{username}</p>
+            <p className={styles["tag"]}>@{tag}</p>
+            <p className={styles["location"]}>{location}</p>
         </div>
-        <ul className="stats">
-        <li>
-            <span className="label">Followers</span>
-            <span className="quantity">{stats.followers}</span>
+        <ul className={styles["stats"]}>
+        <li className={styles["stats-colums"]}>
+            <span className={styles["label"]}>Followers</span>
+            <span className={styles["quantity"]}>{stats.followers}</span>
         </li>
-        <li>
-            <span className="label">Views</span>
-            <span className="quantity">{stats.views}</span>
+        <li className={styles["stats-colums"]}>
+            <span className={styles["label"]}>Views</span>
+            <span className={styles["quantity"]}>{stats.views}</span>
         </li>
-        <li>
-            <span className="label">Likes</span>
-            <span className="quantity">{stats.likes}</span>
+        <li className={styles["stats-colums"]}>
+            <span className={styles["label"]}>Likes</span>
+            <span className={styles["quantity"]}>{stats.likes}</span>
         </li>
         </ul>
     </div>
 );
+
+Profile.propTypes = {
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.shape({
+        followers: PropTypes.number.isRequired,
+        views: PropTypes.number.isRequired,
+        likes: PropTypes.number.isRequired
+    }).isRequired
+};
