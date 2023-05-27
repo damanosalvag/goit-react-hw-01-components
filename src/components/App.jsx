c2d56a10d972e0dd03f8b12c34134539faaa5d21
Dairo Manosalva <38769711+damanosalvag@users.noreply.github.com>
2023-05-26 22:55:13 -0500

    task 2 ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
 import user from '../data/user.json';
+import data from '../data/data.json';
 import { Profile } from './Perfil/index';
+import { Statistics } from './Statistics/Statistics';
 import styles from './Perfil/Perfil.module.css';
 
 
 export const App = () => {
   return (
-    <div className={styles["container"]}>
-      <Profile {...user} />
-    </div>
+      <div className={styles["container"]}>
+        <Profile {...user} />
+        <Statistics title="Upload stats" stats={data}/>
+      </div>
   );
 };
 
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,35 @@
+import styles from './Statistics.module.css';
+import PropTypes from 'prop-types';
+
+function colorRandom() {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  return `rgb(${red}, ${green}, ${blue})`;
+}
+
+
+export const Statistics = ({ stats, title }) => {
+    return (<section className={styles['statistics']}>
+        <h2 className={styles["title"]}>{title}</h2>
+        <ul className={styles["stat-list"]}>
+            {stats.map(stat => { 
+                const randomColor = colorRandom();
+                return stat.id <= "id-4" &&
+                <li className={styles["item"]} key={stat.id} style={{ backgroundColor: randomColor }}>
+                    <span className={styles["label"]}>{ stat.label }</span>
+                    <span className={styles["percentage"]}>{ stat.percentage }%</span>
+                </li>    
+            })}
+        </ul>
+    </section>)
+};
+
+Statistics.propTypes = {
+    title: PropTypes.string.isRequired,
+    stats: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+        percentage: PropTypes.number.isRequired
+    }).isRequired
+};
